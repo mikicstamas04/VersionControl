649209a10010f00d566d0cfb1b9120ba68426ad7
Tamás György Mikics <tamas.mikics@stud.uni-corvinus.hu>
2026-04-07 17:16:40 +0200

    Alapsturktúra létrehozva.

--- UserMaintenance/UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/UserMaintenance/Form1.Designer.cs
@@ -28,12 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            listBox1 = new ListBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            lblLastName = new Label();
+            lblFirstName = new Label();
+            btnAdd = new Button();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(47, 86);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(544, 764);
+            listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(816, 87);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(452, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(816, 245);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(452, 27);
+            textBox2.TabIndex = 2;
+            // 
+            // lblLastName
+            // 
+            lblLastName.AutoSize = true;
+            lblLastName.Location = new Point(705, 94);
+            lblLastName.Name = "lblLastName";
+            lblLastName.Size = new Size(50, 20);
+            lblLastName.TabIndex = 3;
+            lblLastName.Text = "label1";
+            // 
+            // lblFirstName
+            // 
+            lblFirstName.AutoSize = true;
+            lblFirstName.Location = new Point(705, 252);
+            lblFirstName.Name = "lblFirstName";
+            lblFirstName.Size = new Size(50, 20);
+            lblFirstName.TabIndex = 4;
+            lblFirstName.Text = "label2";
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(816, 320);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(131, 44);
+            btnAdd.TabIndex = 5;
+            btnAdd.Text = "button1";
+            btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1314, 988);
+            Controls.Add(btnAdd);
+            Controls.Add(lblFirstName);
+            Controls.Add(lblLastName);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ListBox listBox1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label lblLastName;
+        private Label lblFirstName;
+        private Button btnAdd;
     }
 }
